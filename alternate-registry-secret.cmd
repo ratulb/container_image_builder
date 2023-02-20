a3b6961d54b4de3bbd4678b4691cfe99c8ed262a
Ratul <ratul75@hotmail.com>
2023-02-20 21:07:07 +0530

    Create alternate-registry-secret.cmd
    Alternate way to create docker registry secret

---
+++ alternate-registry-secret.cmd
@@ -0,0 +1,1 @@
+kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=[docker-user-id] --docker-password=[docker-password] --docker-email=[your email]
