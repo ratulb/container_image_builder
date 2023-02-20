11491857cc5cb6fd1780f97fca7129b7e060b813
Ratul <ratul75@hotmail.com>
2023-02-20 21:18:30 +0530

    Update create-image.sh
    Comments

--- create-image.sh
+++ create-image.sh
@@ -1,9 +1,18 @@
+# Delete existing secret
+kubectl delete secrets regcred
+sleep 1
+# Create registry secret
+. registry-secret.cmd
+# Delete kaniko pod
 kubectl delete pod kaniko
 sleep 1
+# Delete volume claim
 kubectl delete persistentvolumeclaims dockerfile-claim
 sleep 1
+# Delete volume
 kubectl delete persistentvolume dockerfile
 sleep 1
+# Recreate every thing
 kubectl apply -f volume.yaml
 sleep 1
 kubectl apply -f volume-claim.yaml
